1a6873c0eb3e12df5d28d9d3878b08176d378279
Vladimir <vladimirbebishev@gmail.com>
2023-09-09 17:53:42 +1000

    delete Tigr

--- src/Main.java
+++ src/Main.java
@@ -11,16 +11,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         /*Animal an = new Tigr();
         an.getName();*/
-        Animal animal = new Animal() {
-            @Override
-            public String getName() {
-                return "Tigr";
-            }
-        };
-        /*System.out.println(animal.getName());
-        Opperationable tre = () -> System.out.println("xew");*/
-        //Stream.of(1,2,3).filter(x -> x>2).forEach(x -> System.out.println(x));
-        Integer[] array = Stream.of(1, 2, 3, 4, 5, 6).toArray(g -> new Integer[g]);
+
     }
 }
 
